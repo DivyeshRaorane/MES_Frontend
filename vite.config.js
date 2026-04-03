6e081db202371d2b7e568fd641c217ad055c5870
Divyesh Raorane <divyeshraorane172@gmail.com>
2026-04-03 09:33:22 +0530

    Change some configuration in vite.config file

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { all } from 'axios'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  server:{
-    host:true,
-    allowedHosts:true
+  plugins: [react(), tailwindcss()],
+  server: {
+    host: "0.0.0.0",
+    port: 5173,
+    strictPort: true,
+    hmr: false,
+    allowedHosts: true
   }
 })
